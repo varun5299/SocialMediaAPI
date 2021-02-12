31c1d62b56fd690ecd2963d009813f928015fd56
varun5299 <varun.agarwal5299@gmail.com>
2021-02-12 15:24:30 +0530

    Added more Annotations

--- src/main/java/com/varun/socialmedia/SocialmediaApplication.java
+++ src/main/java/com/varun/socialmedia/SocialmediaApplication.java
@@ -1,8 +1,17 @@
 package com.varun.socialmedia;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@Configuration
+@EntityScan("com.varun.socialmedia.domain")
+@EnableJpaRepositories("com.varun.socialmedia.repository")
 @SpringBootApplication
 public class SocialmediaApplication {
 
